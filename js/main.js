dc052f66c939e0c1ce3be8bd763aee32d8a344ac
caasi <caasi.igd@gmail.com>
2014-08-12 17:06:17 +0800

    first working prototype

--- js/main.js
+++ js/main.js
@@ -1,17 +1,89 @@
 (function(){
-  var $video, video, tracks, startTime, x$;
-  console.log('foobar');
-  $video = $('video');
-  video = $video.get()[0];
-  tracks = video.textTracks[0];
-  console.log(tracks);
-  startTime = 0;
-  x$ = $video;
-  x$.on('play', function(e){});
-  x$.on('timeupdate', function(e){
-    if (startTime === 0) {
-      startTime = video.currentTime;
+  var ref$, div, ol;
+  ref$ = React.DOM, div = ref$.div, ol = ref$.ol;
+  this.ReactVTT == null && (this.ReactVTT = React.createClass({
+    displayName: 'ReactVTT',
+    className: 'react-vtt',
+    getInitialState: function(){
+      return {
+        attributes: {},
+        media: null,
+        track: null
+      };
+    },
+    componentWillReceiveProps: function(props){
+      console.log(props);
+    },
+    componentDidMount: function(){
+      var $media, $track, track, tracks, i$, ref$, len$, attr, value, update, this$ = this;
+      $media = $('video');
+      if ($media.length === 0) {
+        $media = $('audio');
+      }
+      this.state.media = $media.get()[0];
+      if (!this.state.media) {
+        throw new Error('<video> or <audio> not found');
+      }
+      $track = $media.children(this.props.target);
+      track = $track.get()[0];
+      if (!track) {
+        throw new Error("Target <track>: " + this.props.target + " not found");
+      }
+      tracks = this.state.media.textTracks;
+      for (i$ = 0, len$ = (ref$ = track.attributes).length; i$ < len$; ++i$) {
+        attr = ref$[i$];
+        value = attr.value !== '' ? attr.value : true;
+        this.state.attributes[attr.name] = value;
+      }
+      for (i$ = 0, len$ = tracks.length; i$ < len$; ++i$) {
+        track = tracks[i$];
+        if (track.kind === this.state.attributes.kind && track.label === this.state.attributes.label && track.language === this.state.attributes.srclang) {
+          this.state.track = track;
+          break;
+        }
+      }
+      if (!this.state.track) {
+        throw new Error("Target TextTrack not found");
+      }
+      update = function(){
+        if (this$.isMounted()) {
+          this$.forceUpdate();
+        }
+        return requestAnimationFrame(update);
+      };
+      requestAnimationFrame(update);
+    },
+    render: function(){
+      if (!this.state.track) {
+        return ol();
+      }
+      return ol({
+        className: 'react-vtt active-cues'
+      }, (function(){
+        var i$, to$, results$ = [];
+        for (i$ = 0, to$ = this.state.track.activeCues.length; i$ < to$; ++i$) {
+          results$.push((fn$.call(this, i$)));
+        }
+        return results$;
+        function fn$(i){
+          var cue, delta, ratio;
+          cue = this.state.track.activeCues[i];
+          delta = this.state.media.currentTime - cue.startTime;
+          ratio = delta / (cue.endTime - cue.startTime);
+          return div({
+            key: i,
+            className: 'cue'
+          }, cue.text, div({
+            className: 'actived',
+            style: {
+              width: ratio * 100 + "%"
+            }
+          }, cue.text));
+        }
+      }.call(this)));
     }
-    return console.log(video.currentTime - startTime);
-  });
+  }));
+  React.renderComponent(ReactVTT({
+    target: 'track'
+  }), $('#react-vtt').get()[0]);
 }).call(this);
--- src/ls/main.ls
+++ src/ls/main.ls
@@ -1,29 +1,67 @@
-console.log \foobar
+{div, ol} = React.DOM
 
-# vtt.js
-#vtt-source = $ 'video > track' .attr \src
-#data <- $.get vtt-source
-#string-decoder = WebVTT.StringDecoder!
-#parser = new WebVTT.Parser window, string-decoder
-#  ..onregion = (region) ->
-#    console.log region
-#  ..oncue    = (cue) ->
-#    console.log cue.startTime, cue.endTime
-#  ..parse data
+this.ReactVTT ?=
+  React.createClass do
+    displayName: 'ReactVTT'
+    className: 'react-vtt'
+    getInitialState: ->
+      attributes: {}
+      media: null
+      track: null
+    componentWillReceiveProps: !(props) ->
+      console.log props
+    componentDidMount: !->
+      $media = $ \video
+      if $media.length is 0
+        $media = $ \audio
+      @state.media = $media.get!0
+      if not @state.media
+        throw new Error '<video> or <audio> not found'
+      # find track in tracks
+      $track = $media.children @props.target
+      track = $track.get!0
+      if not track
+        throw new Error "Target <track>: #{@props.target} not found"
+      tracks = @state.media.text-tracks
+      for attr in track.attributes
+        value = if attr.value isnt '' then attr.value else true
+        @state.attributes[attr.name] = value
+      for track in tracks
+        if track.kind     is @state.attributes.kind  and
+           track.label    is @state.attributes.label and
+           track.language is @state.attributes.srclang
+          @state.track = track
+          break
+      if not @state.track
+        throw new Error "Target TextTrack not found"
+      # for better animation, requestAnimationFrame
+      # is better than timeupdate event
+      #$media.on \timeupdate (e) ~> @forceUpdate!
+      update = ~>
+        @forceUpdate! if @isMounted!
+        requestAnimationFrame update
+      requestAnimationFrame update
+    render: ->
+      return ol! if not @state.track # element isnt mounted
+      ol do
+        className: 'react-vtt active-cues'
+        for let i from 0 til @state.track.active-cues.length
+          cue = @state.track.active-cues[i]
+          # the resolution of e.timeStamp in FireFox is 100x than other
+          # browsers and the event fires more frequently, so we should use
+          # video.currentTime instead of e.timeStamp
+          delta = @state.media.current-time - cue.start-time
+          ratio = delta / (cue.end-time - cue.start-time)
+          div do
+            key: i
+            className: 'cue'
+            cue.text
+            div do
+              className: 'actived'
+              style:
+                width: "#{ratio * 100}%"
+              cue.text
 
-# pure video events
-$video = $ \video
-video = $video.get!0
-tracks = video.text-tracks.0
-console.log tracks
-
-start-time = 0
-$video
-  ..on \play (e) ->
-  ..on \timeupdate (e) ->
-    # the resolution of e.timeStamp in FireFox is 100x than other
-    # browsers and the event fires more frequently, so we should use
-    # video.currentTime instead of e.timeStamp
-    start-time := video.current-time if start-time is 0
-    console.log video.current-time - start-time
-
+React.renderComponent do
+  ReactVTT target: 'track'
+  $ \#react-vtt .get!0
